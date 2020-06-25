b3359021dbea53a32c6feda044b0f21ce8f624d3
Wang Wei <wangwei1237@gmail.com>
2020-06-25 10:17:26 +0800

    Site updated: 2020-06-25 10:17:24

--- gittalk-config.js
+++ gittalk-config.js
@@ -1,10 +1,16 @@
+/*gittalk id must be less than 50*/
+var id = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1];
+if (id.length > 50) {
+	id = id.split('_').join('')
+}
+
 var gitalk = new Gitalk({
     "clientID": "7bf1e91eaff35fd004dc",
     "clientSecret": "d151c7bc85d544dafe2c0de75e3a174623229a3a",
     "repo": "wangwei1237.github.io",
     "owner": "wangwei1237",
     "admin": ["wangwei1237"],
-    "id": window.location.pathname.split('/')[window.location.pathname.split('/').length - 1],
+    "id": id, 
     "distractionFreeMode": false
 });
 
